a53d99ec37d2920bc7e67835c1a01814c61e6865
Aubrey Yan <aubreym.yan@gmail.com>
2022-02-22 00:47:59 -0500

    started collection of data

--- datacollection/main.py
+++ datacollection/main.py
@@ -1,9 +1,27 @@
-import praw
+import datetime
+from psaw import PushshiftAPI
+from queries.Queries import Queries
+
+TWELVE_DAYS_INTERVAL = int(datetime.datetime.now().timestamp()) - int((datetime.datetime.now() - datetime.timedelta(days=12)).timestamp())
 
 
 def main():
-    reddit = praw.Reddit("bot", user_agent="bot user agent")
-    print(list(reddit.subreddit("memes").hot(limit=10)))
+    # prw = praw.Reddit("bot", user_agent="bot user agent")
+    reddit = PushshiftAPI()
+
+    ethical_submissions = Queries(reddit)\
+        .query(subreddit="LifeProTips",
+               epoch_delta=1000,
+               epoch_timeinterval=TWELVE_DAYS_INTERVAL,
+               start_epoch=int(datetime.datetime(2022, 1, 1).timestamp()),
+               limit=1000)
+
+    unethical_submissions = Queries(reddit) \
+        .query(subreddit="UnethicalLifeProTips",
+               epoch_delta=1000,
+               epoch_timeinterval=TWELVE_DAYS_INTERVAL,
+               start_epoch=int(datetime.datetime(2022, 1, 1).timestamp()),
+               limit=1000)
 
 
 if __name__ == "__main__":
