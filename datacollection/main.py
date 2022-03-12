be7fd1e4f6873bc7107f39a28e3279067193fbd2
aubreyyan <aubrey@aubreyyan.com>
2022-03-11 22:36:25 -0500

    fixed data fetch and push to mongo

--- datacollection/main.py
+++ datacollection/main.py
@@ -1,27 +1,46 @@
 import datetime
+import praw
+import time
+import json
 from psaw import PushshiftAPI
+from pymongo import MongoClient
+from dotenv import dotenv_values
 from queries.Queries import Queries
 
-TWELVE_DAYS_INTERVAL = int(datetime.datetime.now().timestamp()) - int((datetime.datetime.now() - datetime.timedelta(days=12)).timestamp())
+# TWELVE_DAYS_INTERVAL = int(datetime.datetime.now().timestamp()) - int((datetime.datetime.now() - datetime.timedelta(days=12)).timestamp())
+# ONE_DAY_INTERVAL = int(datetime.datetime.now().timestamp()) - int((datetime.datetime.now() - datetime.timedelta(days=1)).timestamp())
+ONE_HOUR_INTERVAL = int(datetime.datetime.now().timestamp()) - int((datetime.datetime.now() - datetime.timedelta(hours=1)).timestamp())
 
 
 def main():
-    # prw = praw.Reddit("bot", user_agent="bot user agent")
+    config = dotenv_values('.env')
     reddit = PushshiftAPI()
+    praw_reddit = praw.Reddit("bot", user_agent="bot user agent")
+    praw_reddit.config.store_json_result = True
+    client = MongoClient(f"mongodb+srv://{config['MONGODB_USERNAME']}:{config['MONGODB_PASSWORD']}@prd.bfu3q.mongodb.net/test")
+    db = client.prd
+    collection = db.data
 
-    ethical_submissions = Queries(reddit)\
+    start = time.time()
+
+    ethical_submissions = Queries(reddit, praw=praw_reddit)\
         .query(subreddit="LifeProTips",
-               epoch_delta=1000,
-               epoch_timeinterval=TWELVE_DAYS_INTERVAL,
+               epoch_delta=None,
+               epoch_timeinterval=ONE_HOUR_INTERVAL,
                start_epoch=int(datetime.datetime(2022, 1, 1).timestamp()),
-               limit=1000)
+               limit=500000)
 
-    unethical_submissions = Queries(reddit) \
+    unethical_submissions = Queries(reddit, praw=praw_reddit) \
         .query(subreddit="UnethicalLifeProTips",
-               epoch_delta=1000,
-               epoch_timeinterval=TWELVE_DAYS_INTERVAL,
+               epoch_delta=None,
+               epoch_timeinterval=ONE_HOUR_INTERVAL,
                start_epoch=int(datetime.datetime(2022, 1, 1).timestamp()),
-               limit=1000)
+               limit=500000)
+
+    print(f"took {time.time() - start} seconds")
+
+    collection.insert(json.loads(ethical_submissions.df.T.to_json()).values())
+    collection.insert(json.loads(unethical_submissions.df.T.to_json()).values())
 
 
 if __name__ == "__main__":
--- datacollection/queries/Queries.py
+++ datacollection/queries/Queries.py
@@ -1,17 +1,20 @@
 import pandas
 from psaw import PushshiftAPI
+from praw import Reddit
+from typing import Optional
 
 
 class Queries:
 
-    def __init__(self, reddit: PushshiftAPI):
+    def __init__(self, reddit: PushshiftAPI, praw: Reddit):
         """
         :param reddit: api wrapper
         """
         self.reddit = reddit
+        self.praw = praw
         self.df = pandas.DataFrame()
 
-    def query(self, subreddit: str, epoch_delta: int, epoch_timeinterval: int, start_epoch: int, limit: int):
+    def query(self, subreddit: str, epoch_delta: Optional[int], epoch_timeinterval: int, start_epoch: int, limit: int):
         """
         :param subreddit: target subreddit to query
         :param epoch_delta: number of posts to take at a time i.e. limit
@@ -19,12 +22,27 @@ class Queries:
         :param start_epoch: starting time
         :param limit: total limit on submissions
         """
-        idx = 0
+        idx = 1
         while self.df.size < limit:
-            self.df = pandas.concat([self.df, pandas.DataFrame([submission.d_ for submission in self.reddit.search_submissions(
+            submissions = [submission.d_ for submission in self.reddit.search_submissions(
                 after=start_epoch - epoch_timeinterval * idx,
+                before=start_epoch - epoch_timeinterval * (idx - 1),
                 subreddit=subreddit,
-                limit=epoch_delta
-            )])])
+                limit=100
+            )]
+            submissions = [submission for submission in self.praw.info([f"t3_{info['id']}" for info in submissions]) if submission.score > 4 ]
+            valid_submissions = [{
+                "num_awards": submission.total_awards_received,
+                "num_reports": submission.num_reports if submission.num_reports is not None else 0,
+                "score": submission.score,
+                "ups": submission.ups,
+                "upvote_ratio": submission.upvote_ratio,
+                "text": f"{submission.title} {submission.selftext if submission.selftext != '[removed]' else ''}",
+                "ethical_tag": subreddit == "LifeProTips"
+            } for submission in submissions if submission.ups > 4 and submission.removed_by is None]
+            search_df = pandas.DataFrame(valid_submissions)
+            print(f"found {len(search_df)} matching submissions for -{idx} hours...")
+            self.df = pandas.concat([self.df, search_df])
+            idx += 1
 
         return self
